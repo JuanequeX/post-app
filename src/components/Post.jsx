a64a444ab7f124d2a93fefd3c589371bde78ff92
juanequex <ramon.g.uz37@gmail.com>
2023-04-01 23:16:02 -0600

    Enable Delete method

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 
-const Post = ({ post }) => {
+const Post = ({ post, posts, setPosts }) => {
+
+  let URL = 'http://localhost:3000/api/v1'
+
+  const handleDelete = () => {
+    fetch(`${URL}/posts/${post.id}`, {
+      method: 'DELETE'
+    })
+    .then((response) => {
+      console.log(response)
+      setPosts(
+        posts.filter((item) => {
+          return item.id !== post.id
+        })
+      )
+    })
+  }
+
   return (
     <div>
+      <hr />
       <h3>{post.email}</h3>
       <p>{post.comment}</p>
+      <div>
+        <button onClick={() => handleDelete()}>Delete Post</button>
+      </div>
+      <hr />
     </div>
   )
 }
